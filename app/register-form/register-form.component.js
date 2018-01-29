c8bd9dce5f956e7507462c97d84fb502a486a269
Reda Ramadan <king2sairos2013@gmail.com>
2018-01-29 11:10:15 +0200

    chore: make register form an angular component

---
+++ app/register-form/register-form.component.js
@@ -0,0 +1,8 @@
+angular.
+module('registerForm')
+  .component('registerForm', {
+    templateUrl: 'register-form/register-form.template.html',
+    controller: function registerFormController() {
+
+    }
+  });
